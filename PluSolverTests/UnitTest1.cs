f7b37b28043fcee3a3a5e62a611a3b6d1b86ccb6
Sorush <sorush.kh@gmail.com>
2020-06-20 21:28:41 +0100

    Tests folder added with a 2x2 test

---
+++ PluSolverTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PluSolver;
+
+
+namespace PluSolverTests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        private bool IsEqual(double a, double b)
+        {
+            return (Math.Abs(a - b) < 1e-10) ;
+        }
+
+        [TestMethod]
+        public void Test_2Unknows()
+        {
+            double[,] A = new double[,] {
+                { 1,  2},
+                { 3,  1}
+            };
+            var B = new double[] { 5, 5 };
+
+            var solver = new Solver(A, B);
+            double[] X=null;
+
+            try
+            {
+                X = solver.SolveX();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+            
+
+            Assert.IsTrue(IsEqual(X[0], 1) && IsEqual(X[1], 2));
+
+        }
+    }
+}
